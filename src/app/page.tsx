4f1f9409ac2ddc3649df373cff3e9925ae592197
Kwuasimoto <thomasshankgit@gmail.com>
2025-09-04 23:04:30 -0400

    feature is working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,15 @@ export default async function Home() {
 
   return (
     <div className="relative grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <Card className="p-4">
+      <Card className="p-4 bg-card-foreground text-foreground">
         {user ? (
           <div className="flex flex-col gap-2 justify-center">
             <h2>Congratulations! Local is working</h2>
-            <p className="text-secondary">Time for production</p>
-            <pre className="roundedp-4">{JSON.stringify(user, null, 2)}</pre>
+            <p>Time for production</p>
+            <pre className="rounded p-4">{JSON.stringify(user, null, 2)}</pre>
           </div>
         ) : (
-          <span className="rounded bg-gray-900 p-4">
+          <span className="rounded p-4">
             Error!~ Failed to find user by ID {userId} in your db
           </span>
         )}
