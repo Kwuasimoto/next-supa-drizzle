3e50f2c3902f710be53aeb39f3a8d27e3f3ef91a
Kwuasimoto <thomasshankgit@gmail.com>
2025-09-04 22:50:47 -0400

    missed something from starter/drizzle-postgresql

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
-import Counter from "@/components/counter";
-import { Card } from "@/components/ui/card";
+import { db, users } from "@/db";
+import { eq } from "drizzle-orm";
 
-export default function Home() {
+export default async function Home() {
+  const userId = 1;
+
+  const user = await db.query.users.findFirst({
+    where: eq(users.id, userId),
+  });
+
   return (
-    <div className="grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <Card className="mt-4 p-4">
-        <Counter />
-      </Card>
+    <div className="relative grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+      {user ? (
+        <div className="flex flex-col gap-2 justify-center">
+          <h2>Congratulations! Local is working</h2>
+          <p className="text-white/50">Time for production</p>
+          <pre className="rounded bg-gray-900 p-4">
+            {JSON.stringify(user, null, 2)}
+          </pre>
+        </div>
+      ) : (
+        <span className="rounded bg-gray-900 p-4">
+          Error!~ Failed to find user by ID {userId} in your db
+        </span>
+      )}
     </div>
   );
 }
