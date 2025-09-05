0ca72895bf2b8065c927684c50f986b259fe4ad5
Kwuasimoto <thomasshankgit@gmail.com>
2025-09-04 23:20:19 -0400

    added conditional UI to show prod env var is working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { db, users } from "@/db";
+import { isProd } from "@/lib/const";
 import { eq } from "drizzle-orm";
 
 export default async function Home() {
@@ -12,8 +13,13 @@ export default async function Home() {
     <div className="relative grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       {user ? (
         <div className="flex flex-col gap-2 justify-center">
-          <h2>Congratulations! Local is working</h2>
-          <p className="text-white/50">Time for production</p>
+          <h2>Congratulations! {isProd ? "prod" : "local"} is working</h2>
+          {isProd ? (
+            <p className="text-white/80">Have fun!</p>
+          ) : (
+            <p className="text-white/50">Time for production</p>
+          )}
+
           <pre className="rounded bg-gray-900 p-4">
             {JSON.stringify(user, null, 2)}
           </pre>
