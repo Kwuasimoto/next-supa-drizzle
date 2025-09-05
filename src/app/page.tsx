d4252852a6a765b65ef5cb120881b57dec431b74
Kwuasimoto <thomasshankgit@gmail.com>
2025-09-04 21:40:59 -0400

    shadcn + tailwindcss starter working locally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Counter from "@/components/counter";
+import { Card } from "@/components/ui/card";
+
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <h1> Tailwind starter works, wahoo! </h1>
+      <Card className="mt-4 p-4">
+        <Counter />
+      </Card>
     </div>
   );
 }
