d6106b061247b597c892cb248877dbe37d758cbf
Kwuasimoto <thomasshankgit@gmail.com>
2025-09-04 22:32:20 -0400

    remove grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+    <div className="grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <h1> Tailwind starter works, wahoo! </h1>
     </div>
   );
