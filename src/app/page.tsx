fb99882f330275e633ad34b675702bd34e339cf1
Kwuasimoto <thomasshankgit@gmail.com>
2025-09-04 22:59:08 -0400

    checking other style results in other starter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/ui/card";
 import { db, users } from "@/db";
 import { eq } from "drizzle-orm";
 
@@ -10,19 +11,19 @@ export default async function Home() {
 
   return (
     <div className="relative grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      {user ? (
-        <div className="flex flex-col gap-2 justify-center">
-          <h2>Congratulations! Local is working</h2>
-          <p className="text-white/50">Time for production</p>
-          <pre className="rounded bg-gray-900 p-4">
-            {JSON.stringify(user, null, 2)}
-          </pre>
-        </div>
-      ) : (
-        <span className="rounded bg-gray-900 p-4">
-          Error!~ Failed to find user by ID {userId} in your db
-        </span>
-      )}
+      <Card className="p-4">
+        {user ? (
+          <div className="flex flex-col gap-2 justify-center">
+            <h2>Congratulations! Local is working</h2>
+            <p className="text-secondary">Time for production</p>
+            <pre className="roundedp-4">{JSON.stringify(user, null, 2)}</pre>
+          </div>
+        ) : (
+          <span className="rounded bg-gray-900 p-4">
+            Error!~ Failed to find user by ID {userId} in your db
+          </span>
+        )}
+      </Card>
     </div>
   );
 }
