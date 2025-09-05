b14bbb37ae3b87c463ec5653891671b045a419c7
Kwuasimoto <thomasshankgit@gmail.com>
2025-09-04 22:34:02 -0400

    remove grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+    <div className="grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <Card className="mt-4 p-4">
         <Counter />
       </Card>
